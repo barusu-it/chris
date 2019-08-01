a9089c81ddd1ef6825c4044ba7c171ac1cce98ad
barusu <barusu.it@gmail.com>
2019-08-01 14:10:25 +0800

    recode constructor

--- chris-app/src/main/java/it/barusu/chris/query/Blog.kt
+++ chris-app/src/main/java/it/barusu/chris/query/Blog.kt
@@ -5,13 +5,11 @@ import javax.persistence.*
 @Entity
 @Table(name = "blog")
 data class Blog (
-    @Id
-    @GeneratedValue
-    var id: Long = 0,
+        @Id
+        @GeneratedValue
+        var id: Long = 0,
 
-    @Column
-    var title: String?
-){
-    constructor() : this(0, null)
-}
+        @Column
+        var title: String? = null
+)
 
